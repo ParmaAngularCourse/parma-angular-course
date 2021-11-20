5ada57dd5d2260f8937449744d3218116cdc9fa0
Aleksei Kovalev <Kovalev.AI@parma.ru>
2021-11-20 18:51:14 +0500

    Создание компонента news-list

--- block2/src/app/app.module.ts
+++ block2/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NewsBlockComponent } from './news-block/news-block.component';
+import { NewsBlockComponent } from './news-list/news-block/news-block.component';
+import { NewsListComponent } from './news-list/news-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsBlockComponent
+    NewsBlockComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule
