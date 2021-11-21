09a7115a37f6cb6886af5a00f85d6ede99c2cf26
Eduard Gafurov <gafurov@parma.ru>
2021-11-21 17:53:44 +0500

    Add lesson 2

---
+++ lesson2/src/app/all-posts/single-post-detail/single-post-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-post-detail',
+  templateUrl: './single-post-detail.component.html',
+  styleUrls: ['./single-post-detail.component.css']
+})
+export class SinglePostDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
