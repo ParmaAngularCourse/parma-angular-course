ad5a1ea3a38457a0454842c109688715748320c8
aptukov.ainur <aptukov.ainur@gmail.com>
2021-11-25 00:58:55 +0500

    Блок 3 - пункты 1.2.1 и 1.2.2

--- lesson01/src/app/news/news-types.ts
+++ lesson01/src/app/news/news-types.ts
@@ -1,7 +1,23 @@
 export class NewsItemModel {
+
+  private timeZoneMinutes : number = 0;
+
   constructor(public id: number,
-              public date: string,
+              public date: Date,
               public head: string,
-              public desc: string) {
+              public desc: string,
+              public tag: string) {
+    this.timeZoneMinutes = - (new Date()).getTimezoneOffset();
+  }
+
+  public get dateLocal() : string {
+    return new Date(
+      this.date.getFullYear(),
+      this.date.getMonth(),
+      this.date.getDate(),
+      this.date.getHours(),
+      this.date.getMinutes() + this.timeZoneMinutes,
+      this.date.getSeconds()
+    ).toISOString().slice(0, 16);
   }
 }
--- lesson01/src/app/news/news.component.ts
+++ lesson01/src/app/news/news.component.ts
@@ -10,16 +10,28 @@ import {NewsItemModel} from "./news-types";
 export class NewsComponent implements OnInit {
 
   news: NewsItemModel[] = [
-    new NewsItemModel(1, "01.01.2021 00:00:01", "Новость #1", "Текст новости #1"),
-    new NewsItemModel(2, "01.02.2021 00:00:02", "Новость #2", "Текст новости #2"),
-    new NewsItemModel(3, "01.03.2021 00:00:03", "Новость #3", "Текст новости #3"),
-    new NewsItemModel(4, "01.04.2021 00:00:04", "Новость #4", "Текст новости #4")
-  ]
+    new NewsItemModel(1, new Date(2021, 0, 1, 0, 0, 1),
+      "Новость #1", "Текст новости #1", "politic"),
+    new NewsItemModel(2, new Date(2021, 1, 1, 0, 0, 2),
+      "Новость #2", "Текст новости #2","internet"),
+    new NewsItemModel(3, new Date(2021, 2, 1, 0, 0, 3),
+      "Новость #3", "Текст новости #3", "science"),
+    new NewsItemModel(4, new Date(2021, 3, 1, 0, 0, 4),
+      "Новость #4", "Текст новости #4", "tourism")
+  ];
+  tagsList: {tag:string, text:string}[] = [
+    { tag: "politic", text: "Политика" },
+    { tag: "tourism", text: "Туризм" },
+    { tag: "economy", text: "Экономика" },
+    { tag: "science", text: "Наука" },
+    { tag: "internet", text: "Интернет" }
+  ];
 
   showModal: boolean = false;
   editedItem!: NewsItemModel;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -32,17 +44,17 @@ export class NewsComponent implements OnInit {
   }
 
   onEditItem($event: NewsItemModel) {
-    this.editedItem = new NewsItemModel($event.id, $event.date, $event.head, $event.desc);
+    this.editedItem = new NewsItemModel($event.id, $event.date, $event.head, $event.desc, $event.tag);
     this.showModal = true;
   }
 
   onAdd() {
-    this.editedItem = new NewsItemModel(-1, "", "" , "");
+    this.editedItem = new NewsItemModel(-1, new Date(), "" , "", "");
     this.showModal = true;
   }
 
   onDateChange(value: string) {
-    this.editedItem.date = value;
+    this.editedItem.date = new Date(value);
   }
 
   onHeadChange(value: string) {
@@ -53,7 +65,11 @@ export class NewsComponent implements OnInit {
     this.editedItem.desc = value;
   }
 
-  onSaveModal(){
+  onTagsChange(value: string) {
+    this.editedItem.tag = value;
+  }
+
+  onSaveModal() {
     if(this.editedItem.id > 0) {
       let index = this.news.findIndex(p => p.id == this.editedItem.id);
       this.news[index] = this.editedItem;
