cff86d56b5958c7011a33309b1a6ad7ef75078eb
Gabdukaev <gabdukaev@parma.ru>
2021-12-20 10:05:54 +0500

    1

--- lesson1/src/app/news-source.service.ts
+++ lesson1/src/app/news-source.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { NewsItem, NewsContent, Theme } from './news/news-types';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 
 
@@ -24,7 +24,9 @@ export class NewsSourceService {
 
   public getNews() : Observable<NewsItem[]> {
     console.log('GetNews');
-    return this.httpClient.get<NewsData[]>(this._api + "GetNews").pipe(map(raw_items => 
+    return this.httpClient.get<NewsData[]>(this._api + "GetNews", {
+        headers: new HttpHeaders().set('Authorization','fff')
+      }).pipe(map(raw_items => 
       raw_items.map(item => {return { 
                   id: item.id,
                   content: { caption: item.caption, text: item.text, date: item.date, theme: item.theme },
@@ -32,13 +34,18 @@ export class NewsSourceService {
                }})));
   }
 
-  public deleteNews(id?: number){
+  public deleteNews(id?: number){   
     if (id !== undefined){
-      this.allNews = this.allNews.filter(n=> n.id != id);
+       let isSuccess = false;
+       this.httpClient.delete<boolean>(this._api + `DeleteNews/${id}`).subscribe((data: boolean) => isSuccess = data);
+       console.log(`DeleteNews/${id} => ${isSuccess}`);
     } else {
-      this.allNews = this.allNews.filter(n=> !n.checked);
+        this.allNews.filter(n=> n.checked).forEach((el)=>
+          this.httpClient.delete<boolean>(this._api + `DeleteNews/${el.id}`) 
+        );       
     }
   }
+  
 
   public addNews(newItem: NewsContent){
     let maxId = Math.max(... this.allNews.map(n=> n.id), 0);
